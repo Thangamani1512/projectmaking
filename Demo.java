3acafc569f39eeff9bf390feec2534c1d7b447c3
Thangamani <selvajayathangamani@gmail.com>
2025-04-14 14:25:56 +0530

    version1

---
+++ Demo.java
@@ -0,0 +1,10 @@
+public class Demo
+{
+public static void main(String args[])
+{
+ System.out.println("***************************");	
+ System.out.println("HELLO HERE IS NEW OPENING");
+  System.out.println("***************************");	
+ }
+}
+ 
